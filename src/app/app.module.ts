ea653dc4f61c634d8761c99b145f3052f34eb8ca
saffan19 <saffanmd.cs19@bmsce.ac.in>
2020-11-16 12:46:38 +0530

    added create-event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { LoginComponent } from './login/login.component';
-
+import { SignupComponent } from './signup/signup.component';
+import { CreateEventComponent } from './create-event/create-event.component';
+const appRoutes:Routes=[
+  {path:'',component:LoginComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'createevent',component:CreateEventComponent},
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    CreateEventComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
